79149016af27a84f79eb68bacba8874608e15721
Ruben190404 <92361480+Ruben190404@users.noreply.github.com>
2023-01-27 09:37:56 +0100

    ssg

--- Assets/Scripts/Compass.cs
+++ Assets/Scripts/Compass.cs
@@ -5,24 +5,12 @@ using UnityEngine;
 public class Compass : MonoBehaviour
 {
     public Transform target;
-    
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        // transform.LookAt(target);
-        
         var lookPos = target.position - transform.position;
         lookPos.y = 0;
         var rotation = Quaternion.LookRotation(lookPos);
         transform.rotation = Quaternion.Slerp(transform.rotation, rotation, 1f);
-        
     }
     
 }
